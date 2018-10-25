e7fe857fd7c08c47f25ff6a9d823c879c0ffafe4
Agon G <agonxgashi@gmail.com>
2018-10-25 23:10:11 +0200

    redesigned board interface

--- client/src/app/board/board.component.ts
+++ client/src/app/board/board.component.ts
@@ -21,16 +21,15 @@ import * as tippy from 'tippy.js/dist/tippy.all.min.js';
 
     trigger('listAnimation', [
       transition('* => *', [
-
         query(':enter', style({ opacity: 0 }), {optional: true}),
 
-        query(':enter', stagger('100ms', [
+        query(':enter', stagger('50ms', [
           animate('.5s ease-in', keyframes([
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
             style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
           ]))]), {optional: true}),
-          
+
         query(':leave', stagger('50ms', [
           animate('.1s ease-in', keyframes([
             style({opacity: 1, transform: 'translateY(0)', offset: 0}),
--- server/db_schemas/board/list.schema.js
+++ server/db_schemas/board/list.schema.js
@@ -3,6 +3,7 @@ const Schema   = mongoose.Schema;
 
 const ListSchema = new Schema({
     Name: { type: String, default: 'Backlog' },
+    Wip: { type: Number, default: 0 },
     OrderNo: Number,
     Tasks: [ { type: Schema.Types.ObjectId } ]
 });
